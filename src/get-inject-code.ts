da47b09a80a00e7918f44ed38e8c6a33cbbd93e0
zhoulixiang <18366276315@163.com>
2021-11-22 17:08:29 +0800

    feat: 1.0.0

--- src/get-inject-code.ts
+++ src/get-inject-code.ts
@@ -8,31 +8,13 @@ import {
   InjectNodeName,
   InjectCoverInfoName,
 } from './constant';
-const injectFile = path.resolve(__dirname, './inject-code-template.js'); // 编译后会在lib文件夹中
+const jsFile = path.resolve(__dirname, './inject-code-template.js'); // 编译后会在lib文件夹中
 const styleFile = path.resolve(__dirname, './cover.css');
-const DefaultInjectCode = fs.readFileSync(injectFile, 'utf-8');
-const Style = fs.readFileSync(styleFile, 'utf-8');
+const jsCode = fs.readFileSync(jsFile, 'utf-8');
+const styleCode = fs.readFileSync(styleFile, 'utf-8');
 
-const injectCode = (port, options) => {
-  const clickArr = [];
-  const recordArr = [];
-  for (let key in options) {
-    if (key && typeof options[key] === 'function') {
-      clickArr.push(`${key}: ${options[key].toString()}`);
-      recordArr.push(`${key}: false`);
-    }
-  }
-  const code =
-    clickArr.length > 0
-      ? DefaultInjectCode.replace(
-          "'__CODE__CB__'",
-          `'__CODE__CB__', ${clickArr.join(',')}`
-        ).replace(
-          "'__CODE__RECORD__'",
-          `'__CODE__RECORD__', ${recordArr.join(',')}`
-        )
-      : DefaultInjectCode;
-  const _code = code
+const injectCode = (port) => {
+  const code = jsCode
     .replace(/__FILE__/g, InjectPathName)
     .replace(/__LINE__/g, InjectLineName)
     .replace(/__COLUMN__/g, InjectColumnName)
@@ -40,11 +22,8 @@ const injectCode = (port, options) => {
     .replace(/__COVER__/g, InjectCoverName)
     .replace(/__COVERINFO__/g, InjectCoverInfoName)
     .replace(/__PORT__/g, port);
-  const controlList = `<div class="_vc-control-container">
-      <div class="_vc-control-list"></div>
-  </div>`;
 
-  return `<div class="_vc-cover" id="_vc-cover"><div id="_vc-cover-info"></div></div><div id="_vc-control-suspension" draggable="true">V</div>\n<style>${Style}</style>\n<script>\n${_code}\n</script>`;
+  return `<div class="_vc-cover" id="_vc-cover"><div id="_vc-cover-info"></div></div><div id="_vc-control-suspension" draggable="true">V</div>\n<style>${styleCode}</style>\n<script>\n${code}\n</script>`;
 };
 
 export = injectCode;
